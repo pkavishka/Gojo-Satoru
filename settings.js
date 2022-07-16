26d9f1c36a7e9fb94865bcf88ce9ad96a6c8af64
pkavishka <107625822+pkavishka@users.noreply.github.com>
2022-07-16 11:37:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94772097899']
+global.premium = ['94772097899']
+global.ownernomer = '94772097899'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
